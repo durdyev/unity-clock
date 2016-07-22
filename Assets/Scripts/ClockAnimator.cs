b35af71619ba2b009f01879435361079435d012c
Ilya Durdyev <funbanji@gmail.com>
2016-07-22 19:52:19 +0300

    unity-clock

---
+++ Assets/Scripts/ClockAnimator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ClockAnimator : MonoBehaviour {
+
+	public GameObject minutes, hours, seconds;
+	float secondsDegrees = 360f / 60f, minutesDegrees = 360f / 60f, hoursDegrees = 360f / 12f;
+
+	void Update () {
+		DateTime time = DateTime.Now;
+		Debug.Log (time.Minute * minutesDegrees);
+		seconds.transform.localRotation = Quaternion.Euler(new Vector3(0f, 0f, time.Second * -secondsDegrees));
+		minutes.transform.localRotation = Quaternion.Euler(new Vector3(0f, 0f, time.Minute * -minutesDegrees));
+		hours.transform.localRotation = Quaternion.Euler(new Vector3(0f, 0f, time.Hour * -hoursDegrees));
+	}
+}
